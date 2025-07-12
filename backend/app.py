4621db82077c96626da2445988b37d945de89d94
Nisha Rai <152397234+nisharai04@users.noreply.github.com>
2025-07-12 16:03:50 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ def serve_index():
     return send_from_directory('../frontend/pages', 'landing.html')
 
 # Connect to MongoDB
-client = MongoClient('MONGO_URI') #here replcace your mongodb_uri
+client = MongoClient('MONGO_URI') #here replace your mongodb_uri
 db = client['mentalhealth']
 users_collection = db['users']
 
